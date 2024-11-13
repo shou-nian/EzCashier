7e3a4548ee7c3a669d3ae622629bcab1ab0ebf98
riny <riny@aspecta.id>
2024-11-13 15:14:20 +0800

    fix jwt middleware.go

--- app/middleware/middleware.go
+++ app/middleware/middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 	"net/http"
@@ -27,6 +28,9 @@ func JWTMiddleware() gin.HandlerFunc {
 		}
 
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
 			return []byte(os.Getenv("JWT_SECRET_KEY")), nil
 		})
 
@@ -41,6 +45,22 @@ func JWTMiddleware() gin.HandlerFunc {
 	}
 }
 
+// PrivateAuthorizationMiddleware returns a Gin middleware function that performs
+// authorization checks for private routes.
+//
+// This middleware verifies if the user has the necessary credentials to access
+// a protected resource. It checks the JWT token (which should be set in the Gin
+// context by a previous middleware) for the user's admin status and token expiration.
+// If the user is not an admin or the token has expired, the request is aborted
+// with a 401 Unauthorized status.
+//
+// The function doesn't take any parameters directly, but it uses the Gin context
+// to access the JWT token and its claims.
+//
+// Returns:
+//
+//	gin.HandlerFunc: A middleware function that can be used in a Gin router
+//	                 to protect private routes.
 func PrivateAuthorizationMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Check if the user is authorized to access the requested resource
@@ -53,13 +73,13 @@ func PrivateAuthorizationMiddleware() gin.HandlerFunc {
 		claims := token.Claims.(jwt.MapClaims)
 
 		// Set expiration time from JWT data of current user.
-		expires := claims["expires"].(int64)
+		expires := claims["expires"].(float64)
 
 		// Set credential `admin` from JWT data of current user.
 		credential := claims["admin"].(bool)
 
 		// Only user with `admin` credential can create a new user profile.
-		if !credential || now >= expires {
+		if !credential || now >= int64(expires) {
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
